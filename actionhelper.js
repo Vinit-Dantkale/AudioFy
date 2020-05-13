0a1ecd096393c913289d6d45c3839c8e9c06995f
vinit.dantkale@somaiya.edu <vinit.dantkale@somaiya.edu>
2020-05-13 13:32:39 +0530

    changes before adding tooltip
    Merge button option
    
    Merge as per trim sequence

--- actionhelper.js
+++ actionhelper.js
@@ -1,3 +1,15 @@
+var wavesurfer;
+var audioFile;
+var totalAudioDuration;
+var arrBuffer;
+var audioBuffer;
+
+//helper
+function trimDriver(){
+	wavesurfer.enableDragSelection({});
+}
+
+/*
 async function readAndDecodeAudio() {
 	arrBuffer = null;
 	audioBuffer = null;
@@ -218,7 +230,6 @@ function loadAudio() {
 		console.log(audioTracks.childNodes);
 		var tableRow = createAudioRow(new Array(newRegion.id, newRegion.start, newRegion.end));
 		audioTracks.appendChild(tableRow);
-	 	showAndHideMergeOption();
 	});
 	wavesurfer.on('region-update-end', function(newRegion) {
 		document.getElementById(newRegion.id+1).innerText = 
@@ -226,4 +237,111 @@ function loadAudio() {
 		document.getElementById(newRegion.id+2).innerText = 
 			( wavesurfer.getDuration() <= newRegion.end ? wavesurfer.getDuration() : newRegion.end);
 	});
+}
+*/
+
+function showAndHideMergeOption() {
+	var audioTracks = document.getElementById("audio-tracks");
+	var mergeOption = document.getElementById('merge-option');
+	if(audioTracks.childNodes.length >= 4) {
+		mergeOption.setAttribute('class', 'w3-show');
+	} else {
+		mergeOption.setAttribute('class', 'w3-hide');
+	}
+	console.log(mergeOption);
+}
+
+function createAudioRow(arr) {
+	var tableRow = document.createElement("tr");
+	tableRow.setAttribute("id", arr[0]);
+	tableRow.setAttribute("class", "w3-hover-text-green");
+	//tableRow.setAttribute("onmouseover", "highlightRegion('over','"+arr[0]+"')");
+	//tableRow.setAttribute("onmouseleave", "highlightRegion('leave','"+arr[0]+"')");
+	for(var i in arr){
+		var tableData;
+		if(i==0) {
+			tableData = document.createElement("input");
+			tableData.setAttribute("type", "checkbox");
+			tableData.setAttribute("class", "w3-check w3-margin-left");
+		} else {
+			tableData = document.createElement("td");
+			tableData.innerText = arr[i];
+		}
+		tableData.setAttribute("id",arr[0]+i); 	
+		tableRow.appendChild(tableData);
+	}
+
+	var actionsArray = new Array(
+		{"action":"play", "iconClass":"fa fa-play-circle-o"}, 
+		{"action":"download", "iconClass":"fa fa-download"}, 
+		{"action":"delete", "iconClass":"fa fa-times"});
+	for(var i=0; i<actionsArray.length; i++) {
+		var tableData = document.createElement("td");
+		tableData.setAttribute("id", arr[0]+"-"+actionsArray[i].action);
+		var dataIcon = document.createElement("button");
+		dataIcon.setAttribute("title", actionsArray[i].action);
+		dataIcon.setAttribute("class", actionsArray[i].iconClass+" w3-button w3-white w3-border w3-border-light-green w3-round-large");
+		dataIcon.setAttribute("id", arr[0]+"-"+actionsArray[i].iconClass);
+		dataIcon.setAttribute("onClick", actionsArray[i].action+"Track('"+arr[0].toString()+"')"); 	
+		tableData.appendChild(dataIcon);
+		tableRow.appendChild(tableData);
+	}
+	return tableRow;
+}
+
+function highlightRegion(eventName, regionId) {
+	var region = wavesurfer.regions.list[regionId];
+	if(eventName == "over") {
+		region.color = "rgba(0, 255, 0, 0.1)";
+	} else {
+		wavesurfer.regions.list[regionId].color = "rgba(0, 0, 0, 0.1)";
+	}
+}
+
+function playTrack(regionId) {
+	wavesurfer.regions.list[regionId].play();
+}
+
+function mergeTrack() {
+	var audioList = new Array();
+	for( var i in wavesurfer.regions.list ) {
+		var region = wavesurfer.regions.list[i];
+		if( document.getElementById(region.id+'0').checked ) {
+			document.getElementById(region.id+'0').checked = false;
+			audioList.push(wavesurfer.regions.list[i]);
+		}
+	}
+	if(audioList.length >= 2){
+		mergeAudio(audioList);	
+	} else {
+		alert("Select more than 1 tracks");
+	}
+	
+}
+
+function downloadTrack(regionId) {
+	trimAudio(wavesurfer.regions.list[regionId]);
+}
+
+function deleteTrack(regionId) {
+	var track = document.getElementById(regionId);
+	track.parentNode.removeChild(track);
+	wavesurfer.regions.list[regionId].remove();
+	showAndHideMergeOption();
+}
+
+function setPlayButton() {
+	var icon = document.getElementById("play-pause-icon");
+	icon.className = "fa fa-play";
+};
+
+function playAndPause() {
+	var icon = document.getElementById("play-pause-icon");
+	if(icon.className === "fa fa-play"){
+		icon.className = "fa fa-pause";
+		wavesurfer.play();
+	} else {
+		icon.className = "fa fa-play";
+		wavesurfer.pause();
+	}
 }
